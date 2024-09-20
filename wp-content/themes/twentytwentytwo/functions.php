17537e015e48626183f3e667fef6e712d3754480
sufewebsol <sufewebsol@gmail.com>
2024-09-20 23:51:48 +0100

    [INIT]Initial Demo Website

---
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /home/ldoffice/WORKSPACE/PERS PROJECTS/LOCAL-WP/sufewebsol/app/public/wp-content/themes/twentytwentytwo/functions.php:33
+Stack trace:
+#0 {main}
+  thrown in <b>/home/ldoffice/WORKSPACE/PERS PROJECTS/LOCAL-WP/sufewebsol/app/public/wp-content/themes/twentytwentytwo/functions.php</b> on line <b>33</b><br>
